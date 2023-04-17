4802037a06a5c9fa59ea08e27a8f7767e2f39c11
KarthiGovindasamy37 <107383078+KarthiGovindasamy37@users.noreply.github.com>
2023-04-17 13:37:21 +0530

    Socket URL updated

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,4 @@
 export const env = {
-    // api : "http://localhost:3001",
-    socket : "http://localhost:3002",
+    socket : "https://realtime-chat-socket-7q6s.onrender.com",
     api:"https://realtime-chat-uo1a.onrender.com"
 }
